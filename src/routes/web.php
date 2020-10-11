2a65878b775d9e7ac1fc0edff64e70ab67f3f91c
Duncan Morris <duncanmorris07@outlook.com>
2020-10-11 13:28:42 +0200

    Folder structure setup

---
+++ src/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+Route::group(['namespace' => 'duncanrmorris\quotes\Http\Controllers'], function()
+{
+    Route::group(['middleware' => ['web', 'auth']], function(){
+        
+
+   
+
+    });
+});
