a7ffd305c2a779ff4fd56bf5f1bcaa66a33a5b65
Duncan Morris <duncanmorris07@outlook.com>
2020-10-13 15:13:59 +0200

    #1 folder structure setup

---
+++ src/QuotesServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+namespace duncanrmorris\quotes;
+
+use Illuminate\Support\ServiceProvider;
+
+class QuotesServiceProvider extends ServiceProvider
+
+{
+    
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/views','projectsmodule');
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+        
+    }
+
+    public function register()
+    {
+        
+    }
+}
+
