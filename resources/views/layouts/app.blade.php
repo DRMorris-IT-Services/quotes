6d94d2b5be0a6377faa68d3980febe5f7cc4c894
Duncan Morris <d.morris@drmorris-itservices.de>
2021-03-05 11:45:32 +0000

    Side Nav bugfix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,10 @@
     <!-- SIDE BAR -->
     <div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show" id="sidebar">
         <div class="c-sidebar-brand d-md-down-none">
-        <h1><i class="fa fa-money" ></i> &nbsp; Quotes</h1>
+        <h1>
+        <i class="fa fa-money c-sidebar-brand-minimized" ></i>
+        <i class="fa fa-money c-sidebar-brand-full" width="118" height="46" > &nbsp; Quotes</i>
+        </h1>
         
         
         </div>
@@ -43,6 +46,8 @@
             
         
         </ul>
+
+        <button class="c-sidebar-minimizer c-class-toggler" type="button" data-target="_parent" data-class="c-sidebar-minimized"></button>
         
         @endguest
         
